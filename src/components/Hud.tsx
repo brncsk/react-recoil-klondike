75519ebbfb9ac00e88c6f5203f3ea797e9687e9d
Ádám Barancsuk <adam.barancsuk@gmail.com>
2023-01-05 14:44:41 +0100

    feat(Hud): add undo/redo buttons

--- src/components/Hud.tsx
+++ src/components/Hud.tsx
@@ -1,11 +1,21 @@
+import { useContext } from "react";
+
 import { useNewGame } from "../hooks/game";
+import { HistoryContext } from "../util/history";
 
 export function Hud() {
   const newGame = useNewGame();
+  const { canUndo, canRedo, undo, redo } = useContext(HistoryContext);
 
   return (
     <div className="hud">
       <button onClick={newGame}>Deal New</button>
+      <button onClick={undo} disabled={!canUndo}>
+        Undo
+      </button>
+      <button onClick={redo} disabled={!canRedo}>
+        Redo
+      </button>
     </div>
   );
 }
