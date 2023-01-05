3a7acfd43f74f71c71aa1d25f47d24654f6ea0b8
Ádám Barancsuk <adam.barancsuk@gmail.com>
2023-01-05 11:21:27 +0100

    refactor: use `{tableau|foundation}Stack()` util in the respective components

--- src/components/stacks/Foundation.tsx
+++ src/components/stacks/Foundation.tsx
@@ -1,10 +1,11 @@
 import { canDropOntoFoundation, useStack } from "../../hooks/stacks";
+import { foundationStack } from "../../util/stacks";
 
 export function Foundation({ n }: { n: number }) {
   return (
     <div
       {...useStack({
-        stack: `foundation-${n}`,
+        stack: foundationStack(n),
         gridColumn: 3 + n,
         canDrop: canDropOntoFoundation,
       })}
--- src/components/stacks/Tableau.tsx
+++ src/components/stacks/Tableau.tsx
@@ -1,10 +1,11 @@
 import { canDropOntoTableau, useStack } from "../../hooks/stacks";
+import { tableauStack } from "../../util/stacks";
 
 export function Tableau({ n }: { n: number }) {
   return (
     <div
       {...useStack({
-        stack: `tableau-${n}`,
+        stack: tableauStack(n),
         gridColumn: n,
         canDrop: canDropOntoTableau,
       })}
