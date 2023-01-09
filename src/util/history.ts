eaf2978ddc84340493860e3e300150636f4c5d03
Ádám Barancsuk <adam.barancsuk@gmail.com>
2023-01-09 11:07:25 +0100

    feat(history): dump snapshot diffs to the console in development mode

--- src/util/history.ts
+++ src/util/history.ts
@@ -2,6 +2,7 @@ import { createContext } from "react";
 import { Snapshot } from "recoil";
 
 import { HistoryContextType, HistoryState } from "../types";
+import { isDevelopment } from "./env";
 
 export const HistoryContext = createContext<HistoryContextType>({
   undo: () => {},
@@ -15,6 +16,8 @@ const RETAINABLE_NODE_PREFIXES = ["card-stack"];
 
 /** Returns true if the snapshot contains any nodes that should be retained. */
 export function isSnapshotToBeRetained(snapshot: Snapshot) {
+  isDevelopment && dumpSnapshotToConsole(snapshot);
+
   for (const node of snapshot.getNodes_UNSTABLE({ isModified: true })) {
     const retainable = RETAINABLE_NODE_PREFIXES.some((prefix) =>
       node.key.startsWith(prefix)
@@ -44,4 +47,16 @@ export function pushHistoryFrame(
     stack: [{ snapshot, release }, ...state.stack],
     pointer: 0,
   };
+}
+
+/** Dumps the modified contents of a snapshot to the console. */
+export function dumpSnapshotToConsole(snapshot: Snapshot) {
+  console.groupCollapsed(`Snapshot ${snapshot.getID()} at ${Date()}`);
+
+  for (const node of snapshot.getNodes_UNSTABLE({ isModified: true })) {
+    const loadable = snapshot.getLoadable(node);
+    if (loadable.state === "hasValue") console.log(node.key, loadable.contents);
+  }
+
+  console.groupEnd();
 }
