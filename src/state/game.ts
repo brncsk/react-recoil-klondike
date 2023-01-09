65f5b2885f5bac55e763d802298ac274491e6ea5
Ádám Barancsuk <adam.barancsuk@gmail.com>
2023-01-09 09:08:54 +0100

    feat(game): add state representing whether a game is trivially winnable

--- src/state/game.ts
+++ src/state/game.ts
@@ -1,6 +1,37 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+import { NUM_TABLEAU_STACKS } from "../const";
+import { tableauStack } from "../util/stacks";
+import { tableauNumFaceUpCardsState } from "./cards";
+import { stackCardsState } from "./stacks";
 
 export const gameIsWonState = atom({
   key: "game-is-won",
   default: false,
+});
+
+/**
+ * Returns whether the game is trivially winnable.
+ * A game is trivially winnable if the deck is empty and all tableau stacks are
+ * fully face-up.
+ */
+export const gameIsTriviallyWinnableState = selector({
+  key: "game-is-trivially-winnable",
+  get: ({ get }) => {
+    const deckCardCount = get(stackCardsState("deck"));
+
+    if (deckCardCount.length !== 0) {
+      return false;
+    }
+
+    for (let i = 1; i <= NUM_TABLEAU_STACKS; i++) {
+      const tableauCardCount = get(stackCardsState(tableauStack(i)));
+      const numFaceUpCards = get(tableauNumFaceUpCardsState(i));
+
+      if (tableauCardCount.length !== numFaceUpCards) {
+        return false;
+      }
+    }
+
+    return true;
+  },
 });
