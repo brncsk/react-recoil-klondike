65ca2441570156bc5d2a128e440aea4a7446e580
Ádám Barancsuk <adam.barancsuk@gmail.com>
2023-01-09 09:09:04 +0100

    feat(hud): add debug actions

--- src/components/Hud.tsx
+++ src/components/Hud.tsx
@@ -1,11 +1,19 @@
 import { useContext } from "react";
+import { useRecoilValue } from "recoil";
 
-import { useNewGame } from "../hooks/game";
+import { useNewGame, useWinAnimation } from "../hooks/game";
+import { gameIsTriviallyWinnableState, gameIsWonState } from "../state/game";
 import { HistoryContext } from "../util/history";
+
+const isDev = process.env.NODE_ENV === "development";
 
 export function Hud() {
   const newGame = useNewGame();
+  const performWinAnimation = useWinAnimation();
   const { canUndo, canRedo, undo, redo } = useContext(HistoryContext);
+
+  const isTriviallyWinnable = useRecoilValue(gameIsTriviallyWinnableState);
+  const isGameWon = useRecoilValue(gameIsWonState);
 
   return (
     <div className="hud">
@@ -16,6 +24,15 @@ export function Hud() {
       <button onClick={redo} disabled={!canRedo}>
         Redo
       </button>
+      {isDev && (
+        <>
+          <button onClick={performWinAnimation}>Win</button>
+          <div>
+            Triv: {isTriviallyWinnable ? "Yes" : "No"} <br />
+            Won: {isGameWon ? "Yes" : "No"}
+          </div>
+        </>
+      )}
     </div>
   );
 }
