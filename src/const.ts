34d17d87430ce5042825b5ce6968cbc6dc6e7e94
Ádám Barancsuk <adam.barancsuk@gmail.com>
2023-01-09 11:36:53 +0100

    feat(const): add STACKS to enumerate all stacks

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,5 @@
-import { Suit, Rank } from "./types";
+import { Suit, Rank, Stack } from "./types";
+import { foundationStack, tableauStack } from "./util/stacks";
 
 export const NUM_TABLEAU_STACKS = 7;
 export const NUM_FOUNDATION_STACKS = Object.keys(Suit).length;
@@ -6,3 +7,10 @@ export const NUM_CARDS_PER_SUIT = Object.keys(Rank).length;
 
 export const TABLEAU_FANOUT_OFFSET_FACE_DOWN_RATIO = 10;
 export const TABLEAU_FANOUT_OFFSET_FACE_UP_RATIO = 5;
+
+export const STACKS: Stack[] = [
+  "deck",
+  "waste",
+  ...[...Array(NUM_FOUNDATION_STACKS)].map((_, i) => foundationStack(i)),
+  ...[...Array(NUM_TABLEAU_STACKS)].map((_, i) => tableauStack(i)),
+];
