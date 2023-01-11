17066601740418102b8135423f75971ad8fa6522
Ádám Barancsuk <adam.barancsuk@gmail.com>
2023-01-11 15:30:22 +0100

    docs(state/game): add doc comments

--- src/state/game.ts
+++ src/state/game.ts
@@ -6,15 +6,19 @@ import { tableauStack } from "../util/stacks";
 import { tableauNumFaceUpCardsState } from "./cards";
 import { stackCardsState } from "./stacks";
 
+/** Whether the game has started (i.e. the first valid move has been made). */
 export const gameStartedState = atom({
   key: "game-started",
   default: false,
 });
 
+/** Whether the game is paused. */
 export const gamePausedState = atom({
   key: "game-paused",
   default: false,
   effects: [
+    // Pause the game when the browser tab is hidden or the window is not
+    // in focus.
     ({ setSelf }) => {
       document.addEventListener("visibilitychange", () =>
         setSelf(document.hidden)
@@ -26,16 +30,19 @@ export const gamePausedState = atom({
   ],
 });
 
+/** The number of seconds elapsed since the game started. */
 export const gameElapsedSecondsState = atom({
   key: "game-elapsed-time",
   default: 0,
 });
 
+/** The number of moves made in the game. */
 export const gameMovesState = atom({
   key: "game-moves",
   default: 0,
 });
 
+/** Whether the game is won. */
 export const gameIsWonState = atom({
   key: "game-is-won",
   default: false,
@@ -51,6 +58,7 @@ export const gameIsTriviallyWinnableState = selector({
   get: ({ get }) => {
     const deckCardCount = get(stackCardsState("deck"));
 
+    // Check if the deck is empty.
     if (deckCardCount.length !== 0) {
       return false;
     }
@@ -59,6 +67,14 @@ export const gameIsTriviallyWinnableState = selector({
     const wasteCards = get(stackCardsState("waste"));
 
     for (let i = 0; i < wasteCards.length - 1; i++) {
+      // The card [i] (which is below the card [i + 1] in the waste stack)
+      // should have a higher-or-equal rank than the card [i + 1].
+      //
+      // Higher ranks on top might block lower ranks from being moved to the
+      // foundation thus making the game unwinnable by auto-move.
+      //
+      // The direction of the comparison is reversed so we can bail at the
+      // first non-descending pair.
       if (
         getCardRankIndex(wasteCards[i]) <= getCardRankIndex(wasteCards[i + 1])
       ) {
@@ -66,6 +82,7 @@ export const gameIsTriviallyWinnableState = selector({
       }
     }
 
+    // Check if all tableau stacks are fully face-up.
     for (let i = 1; i <= NUM_TABLEAU_STACKS; i++) {
       const tableauCardCount = get(stackCardsState(tableauStack(i)));
       const numFaceUpCards = get(tableauNumFaceUpCardsState(i));
